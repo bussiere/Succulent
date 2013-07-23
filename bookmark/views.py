63fb18b4a4067233121f001ac79e0ba89d33855c
bussiere <bussiere@gmail.com>
2013-07-23 08:35:01 +0200

    en cours

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect,HttpRequest
 from django.template import RequestContext, loader
 from bookmark.form import BookmarkForm
 
@@ -9,15 +9,20 @@ def index(request):
 
 
 def popup(request):
+    try : 
+        url = request.META['HTTP_REFERER']
+    except :
+        url = request.GET['url']
     if request.method == 'POST': # If the form has been submitted...
         form = BookmarkForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
             print form.cleaned_data['Url']
 
             return HttpResponseRedirect('../close/') # Redirect after POST
+    form = BookmarkForm(initial={'Url': url})
     template = loader.get_template('popup.html')
     context = RequestContext(request, {
-        'form': BookmarkForm(),
+        'form': form ,
     })
     return HttpResponse(template.render(context))
 
