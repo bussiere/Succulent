6559f3fd0dd7f723ee8f04e614a40ad862814737
bussiere <bussiere@gmail.com>
2013-07-23 08:44:04 +0200

    rajout

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse,HttpResponseRedirect,HttpRequest
 from django.template import RequestContext, loader
 from bookmark.form import BookmarkForm
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
 
 def index(request):
     template = loader.get_template('index.html')
@@ -13,13 +16,15 @@ def popup(request):
         url = request.META['HTTP_REFERER']
     except :
         url = request.GET['url']
+    soup = BeautifulSoup(urllib2.urlopen(url))
+    title = soup.title.string
     if request.method == 'POST': # If the form has been submitted...
         form = BookmarkForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
             print form.cleaned_data['Url']
 
             return HttpResponseRedirect('../close/') # Redirect after POST
-    form = BookmarkForm(initial={'Url': url})
+    form = BookmarkForm(initial={'Url': url,'Title':title})
     template = loader.get_template('popup.html')
     context = RequestContext(request, {
         'form': form ,
